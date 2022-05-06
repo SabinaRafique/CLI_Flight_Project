24176ca249b0f5ea17a186497991a8b85d320bac
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-05-06 12:30:24 +0100

    Updated formatting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,35 +12,37 @@ public class Main {
         Service service = new Service();
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Welcome to BNTA's Flight Booker!");
+
+        //-------------------------------- Menu (Hamza/Mohamed) -------------------------------\\
 
         boolean exit = false;
-        int userSelection;
+        int menuChoice;
         while (!exit) {
-            System.out.println("Welcome to BNTA's Flight Booker! :)");
             System.out.println("Please select one of the options below:");
             System.out.println("[1] Add a new Passenger");
-            System.out.println("[2] Cancel Flight");
+            System.out.println("[2] Add new Flight");
             System.out.println("[3] Display All Flights");
-            System.out.println("[4] Add new Flight");
+            System.out.println("[4] Cancel Flight");
             System.out.println("[5] Book passenger onto flight");
             System.out.println("[6] Remove Passenger from Flight");
             System.out.println("[7] Exit");
             System.out.println("Please select a number to choose an option:");
 
-            userSelection = scanner.nextInt();
+            menuChoice = scanner.nextInt();
 
-            switch (userSelection) {
+            switch (menuChoice) {
                 case 1:
                     service.addPassenger();
                     break;
                 case 2:
-                    service.cancelFlight();
+                    service.addFlight();
                     break;
                 case 3:
                     service.displayAvailableFlights();
                     break;
                 case 4:
-                    service.addFlight();
+                    service.cancelFlight();
                     break;
                 case 5:
                     service.bookPassengerOnFlight();
